c95ca42ee2f062cb0c5295043972760ada6454c1
Yash Dedhia <yashdedhia05@gmail.com>
2026-01-15 16:53:56 +0000

    [LeetCode Sync] Runtime - 1 ms (64.38%), Memory - 43.4 MB (23.22%)

---
+++ solutions/0782-jewels-and-stones/solution.java
@@ -0,0 +1,39 @@
+	import java.util.HashSet;
+
+	 
+
+	class Solution {
+
+	    public int numJewelsInStones(String jewels, String stones) {
+
+	        HashSet<Character> jewelSet = new HashSet<>();
+
+	        for (char j : jewels.toCharArray()) {
+
+	            jewelSet.add(j);
+
+	        }
+
+	 
+
+	        int count = 0;
+
+	        for (char s : stones.toCharArray()) {
+
+	            if (jewelSet.contains(s)) {
+
+	                count++;
+
+	            }
+
+	        }
+
+	 
+
+	        return count;
+
+	    }
+
+	}
+
+	 
