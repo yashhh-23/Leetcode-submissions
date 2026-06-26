8dace480e0798bf98c86f820958b45e8721d8d97
Yash Dedhia <yashdedhia05@gmail.com>
2026-06-26 17:07:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.9 MB (78.27%)

---
+++ solutions/0098-validate-binary-search-tree/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */class Solution {
+    boolean flag;
+    TreeNode prev;
+    
+    public boolean isValidBST(TreeNode root) {
+        this.flag = true;
+        helper(root);
+        return flag;
+    }
+
+    private void helper(TreeNode root){
+        if(root == null) return;
+
+        helper(root.left);
+
+        if(prev != null && prev.val >= root.val){
+            flag = false;
+        }
+
+        prev = root;
+        helper(root.right);
+    }
+}
+
+
